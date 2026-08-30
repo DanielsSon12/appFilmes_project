1208e7c963ec52c1eeba4308d0da981ed6ad6bdd
Daniel_Ouver <DANIELOUVERNEY67@GMAIL.COM>
2026-08-30 16:45:30 -0300

    style: melhora estilização e responsividade dos cards de filmes

--- src/components/CardFilme/CardFilme.tsx
+++ src/components/CardFilme/CardFilme.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'motion/react'
+
 import type { Filme } from '@/types/filme'
 
 import StarsRating from '../StarsRating/StarsRating'
@@ -8,28 +10,68 @@ interface CardFilmeProps {
 
 const CardFilme = ({ filmes }: CardFilmeProps) => {
 	return (
-		<div className="grid grid-cols-1 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+		<motion.div
+			initial={{ y: 120, opacity: 0 }}
+			animate={{ y: 0, opacity: 1 }}
+			transition={{ duration: 1, ease: 'easeInOut' }}
+			className="m-auto grid max-w-6xl grid-cols-1 items-center gap-5 pt-10 pb-10 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"
+		>
 			{filmes.map((filme) => (
-				<div key={filme.id} className="relative h-full w-full">
-					<div className="group relative aspect-2/3 w-full overflow-hidden rounded-md">
-						<img
-							src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
-							alt={filme.title}
-							className="relative h-full w-full rounded-md object-cover"
-						/>
-					</div>
+				<motion.div
+					key={filme.id}
+					initial="start"
+					whileHover="final"
+					variants={{
+						start: {
+							scale: 1,
+						},
+						final: {
+							scale: 1.05,
+						},
+					}}
+					className="relative aspect-2/3 h-full w-full overflow-hidden rounded-md"
+				>
+					<motion.img
+						variants={{
+							start: {
+								scale: 1,
+							},
+							final: {
+								scale: 1.1,
+							},
+						}}
+						transition={{ duration: 0.3 }}
+						src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+						alt={filme.title}
+						className="h-full w-full rounded-md object-cover"
+					/>
+
+					<div className="pointer-events-none absolute inset-0 bg-linear-to-t from-black/90 via-black/40 to-transparent" />
 
 					<div className="absolute inset-0 flex flex-col justify-end p-3">
 						<h1 className="text-lg text-white">{filme.title}</h1>
-						<StarsRating rating={filme.vote_average} />
 
-						<div>
+						<StarsRating rating={filme.vote_average} />
+						<motion.div
+							variants={{
+								start: {
+									height: 0,
+									opacity: 0,
+								},
+								final: {
+									height: 'auto',
+									opacity: 1,
+								},
+							}}
+							transition={{ duration: 0.3 }}
+							className="overflow-hidden"
+						>
 							<p className="mt-2 line-clamp-3 text-sm text-amber-100">{filme.overview}</p>
-						</div>
+						</motion.div>
 					</div>
-				</div>
+				</motion.div>
 			))}
-		</div>
+		</motion.div>
 	)
 }
 
